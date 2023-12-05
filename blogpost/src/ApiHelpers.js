a396caa4520114cd84a2249ceb80e758b8598bd6
HIMAL L R <117270050+himallr@users.noreply.github.com>
2023-12-05 15:56:08 +0530

    Update ApiHelpers.js

--- blogpost/src/ApiHelpers.js
+++ blogpost/src/ApiHelpers.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 
 export const sendUsers = async (datas, signup) => {
     console.log(signup);
-    const res = await axios.post(`http://localhost:3001/user/${!signup ? "addUser" : "login"}`, {
+    const res = await axios.post(`https://blog-post-frontend-psi.vercel.app/user/${!signup ? "addUser" : "login"}`, {
         name: !signup ? datas.name : "",
         email: datas.email,
         password: datas.password
@@ -20,7 +20,7 @@ export const sendUsers = async (datas, signup) => {
 
 export const addBlogs = async (datas) => {
     console.log(datas);
-    const res = await axios.post("http://localhost:3001/blog/addblogs", {
+    const res = await axios.post("https://blog-post-frontend-psi.vercel.app/blog/addblogs", {
         title: datas.title,
         description: datas.description,
         image: datas.image,
@@ -37,7 +37,7 @@ export const addBlogs = async (datas) => {
 };
 
 export const getBlogs = async () => {
-    const res = await axios.get("http://localhost:3001/blog/get")
+    const res = await axios.get("https://blog-post-frontend-psi.vercel.app/blog/get")
         .catch((e) => console.log(e))
     
     if (res.status !== 200) {
@@ -49,7 +49,7 @@ export const getBlogs = async () => {
 }
 
 export const getPagination = async (p) => {
-    const res = await axios.get(`http://localhost:3001/blog/getbyPagination?p=${p}`)
+    const res = await axios.get(`https://blog-post-frontend-psi.vercel.app/blog/getbyPagination?p=${p}`)
         .catch((e) => console.log(e))
     
     if (res.status !== 200) {
@@ -69,7 +69,7 @@ export const getblogsById = async (id) => {
   };
 
 export const deleteBlogs = async (id) => {
-    const res = await axios.delete(`http://localhost:3001/blog/deleteBlogs/${id}`)
+    const res = await axios.delete(`https://blog-post-frontend-psi.vercel.app/blog/deleteBlogs/${id}`)
         .catch((e) => console.log(e))
 
     if (res.status !== 200) {
@@ -81,7 +81,7 @@ export const deleteBlogs = async (id) => {
 
 export const updateBlogs = async (datas,id) => {
     console.log(id);
-    const res = await axios.put(`http://localhost:3001/blog/updateBlogs/${id}`, {
+    const res = await axios.put(`https://blog-post-frontend-psi.vercel.app/blog/updateBlogs/${id}`, {
         title: datas.title,
         description: datas.description,
         image: datas.image,
@@ -97,7 +97,7 @@ export const updateBlogs = async (datas,id) => {
 
 export const sendRequest = async (id) => {
     const res = await axios
-        .get(`http://localhost:3001/blog/getUserBlog/${id}`)
+        .get(`https://blog-post-frontend-psi.vercel.app/blog/getUserBlog/${id}`)
         .catch((err) => console.log(err));
 
         console.log(res.data);
